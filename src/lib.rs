ac48ddbcd25416ccd5d2ad460fff32642f09a424
Eddio0141 <eddio0141@gmail.com>
2022-04-24 06:47:07 +0100

    finished cli base

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,19 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
+    /// Installs the required files for the tool.
+    ///
+    /// - Requires the 'steamapi.dll' file to be the default dll as when you installed the game.
+    /// - This command will download the required files as well as creating files for the tool to use.
+    /// - Needs to be run before the tool can be used.
+    // TODO verify if all steam_api.dll hash are the same, then remove the first doc comment line
     Install {
-        projects_dir_name: String,
+        #[clap(long)]
+        projects_dir_name: Option<String>,
     },
+    /// Create a new project.
+    ///
+    /// - The project is created in the 'tas' directory, but can be changed through the config file.
     New {
         #[clap(long, short = 'n')]
         project_name: String,
@@ -20,7 +30,12 @@ pub enum Commands {
         // TODO depends on if game_name is set
         #[clap(long, short)]
         copy_game_dir_for_sim_client: bool,
+        #[clap(long, short)]
+        copy_cfgs: bool,
     },
+    /// Initializes a new project in an existing directory.
+    ///
+    /// - This is the same as 'new' but it uses an existing directory.
     Init {
         #[clap(long, short = 'n')]
         folder_name: String,
@@ -30,7 +45,19 @@ pub enum Commands {
         #[clap(long, short)]
         copy_game_dir_for_sim_client: bool,
     },
+    /// Lists all available games.
+    ///
+    /// - Lists all games installed in the 'Half-Life' directory.
+    /// - A game is usually all directories in the Half-Life directory.
+    /// - Able to set exclusions in the config file.
+    // TODO auto detect if its a game or unrelated dir
     Games,
+    /// Generates the .cfg files for TASing.
+    ///
+    /// - Creates the cfg files 'hltas.cfg', 'ingame.cfg', 'record.cfg', 'editor.cfg' and 'cam.cfg'.
+    /// - These files will create keybinds for you to use while TASing, unless the 'minimum_cfgs' flag is set.
+    /// - Read the comment in the config file for more information on what each one does.
+    /// - If the files are already present in 'cfgs/' they will be used instead of creating new ones.
     GenCfgs {
         #[clap(long)]
         minimum_cfgs: bool,

23de8c2b32aca9e17b64e35da0908ac1e662e497
Eddio0141 <eddio0141@gmail.com>
2022-04-25 06:01:27 +0100

    writing new command, unfinished

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
 pub mod cfg;
+pub mod files;
 pub mod helper;
 
-use std::fs::{self};
+use std::{
+    fs::{self, File},
+    io::Write,
+    process::Command,
+};
 
 use anyhow::{bail, Context, Result};
 use clap::{Parser, Subcommand};
-use helper::root_dir;
+use fs_extra::dir::CopyOptions;
+use helper::{half_life_dir, root_dir};
 
 use crate::cfg::Cfg;
+
+// TODO other os support
 
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 
@@ -41,8 +49,14 @@ pub enum Commands {
         // TODO depends on if game_name is set
         #[clap(long, short)]
         copy_game_dir_for_sim_client: bool,
-        #[clap(long, short)]
-        copy_cfgs: bool,
+        #[clap(long, conflicts_with = "no-init-git")]
+        init_git: bool,
+        #[clap(long)]
+        no_init_git: bool,
+        #[clap(long, conflicts_with = "no-copy-hltas-cfgs")]
+        copy_hltas_cfgs: bool,
+        #[clap(long)]
+        no_copy_hltas_cfgs: bool,
     },
     /// Initializes a new project in an existing directory.
     ///
@@ -144,8 +158,159 @@ pub fn run(cli: Cli) -> Result<()> {
             project_name,
             game_name,
             copy_game_dir_for_sim_client,
-            copy_cfgs,
-        } => todo!(),
+            init_git,
+            no_init_git,
+            copy_hltas_cfgs,
+            no_copy_hltas_cfgs,
+        } => {
+            // TODO check requirements of command before running it
+            // load config
+            let cfg = helper::cfg_dir()?;
+            let cfg = Cfg::load_from_path(cfg)?;
+
+            // create project dir
+            let root_dir = helper::root_dir()?;
+            let project_dir = root_dir.join(cfg.project_dir_name).join(project_name);
+            let game_name_full = game_name.as_ref().unwrap_or(&cfg.default_game);
+            let half_life_dir = helper::half_life_dir()?;
+            let game_dir = half_life_dir.join(game_name_full);
+
+            if project_dir.exists() {
+                bail!("Project folder already exists\nHelp: Use 'init' to initialize a project in an existing folder.");
+            } else {
+                fs::create_dir(&project_dir).context("Failed to create project folder")?;
+            }
+
+            let init_git = {
+                if *init_git {
+                    true
+                } else if *no_init_git {
+                    false
+                } else {
+                    cfg.init_git_on_project
+                }
+            };
+
+            if init_git {
+                Command::new("git")
+                    .current_dir(&project_dir)
+                    .arg("init")
+                    .output()
+                    .context("Failed to init git\nHelp: Use '--no-init-git' to skip git init\nNote: This process failing could be due to git not being installed")?;
+
+                // add hardlink hook to .git/hooks/post-checkout
+                let post_checkout_hook_path = project_dir.join(".git/hooks/post-checkout");
+
+                files::write_hard_link_shell_hook(post_checkout_hook_path)?;
+
+                // create .gitignore file
+                let gitignore_path = project_dir.join(".gitignore");
+                let gitignore = "*.bat";
+                let gitignore = format!("\n{gitignore}");
+
+                let mut gitignore_file = if gitignore_path.is_file() {
+                    // append to .gitignore
+
+                    fs::OpenOptions::new()
+                        .append(true)
+                        .open(gitignore_path)
+                        .context("Failed to open .gitignore")?
+                } else {
+                    File::create(gitignore_path).context("Failed to create .gitignore")?
+                };
+
+                gitignore_file
+                    .write_all(gitignore.as_bytes())
+                    .context("Failed to write to .gitignore")?;
+            }
+
+            // copy cfgs if needed to
+            if cfg.copy_cfgs_to_new_game {
+                files::write_cfgs(&project_dir)?;
+            }
+
+            // TODO do this process in the install command, since the whole dir is copied
+            // let client_dll_path = game_dir.join("cl_dlls").join("client.dll");
+
+            // // only if client.dll exists, we copy the game dir
+            // if *copy_game_dir_for_sim_client && client_dll_path.is_file() {
+            //     // copy the game dir for the main client
+            //     let copy_path = game_dir
+            //         .parent()
+            //         .unwrap()
+            //         .join(format!("{game_name}_{}", &cfg.no_client_dll_dir_name));
+
+            //     if game_dir.is_dir() {
+            //         fs::copy(&game_dir, &copy_path).context("Failed to copy game dir")?;
+
+            //         // remove the client.dll
+            //         let copied_client_dll = copy_path.join("cl_dlls").join("client.dll");
+
+            //         fs::remove_file(copied_client_dll).context("Failed to remove client.dll")?;
+            //     } else {
+            //         bail!("Game directory not found");
+            //     }
+            // } else {
+            //     // generate toggle sim client batch files
+            //     files::write_toggle_sim_client(&project_dir)?;
+            // }
+
+            // if this is a non-default game, check if cl_dlls/client.dll exists
+            if let Some(game_name) = game_name {
+                if *game_name != cfg.default_game {
+                    let client_dll_path = game_dir.join("cl_dlls").join("client.dll");
+
+                    // if it exists, copy to the secondary game instance folder, or generate toggle batch files
+                    if client_dll_path.is_file() {
+                        match &cfg.no_client_dll_dir_name {
+                            Some(no_client_dll_dir_name) => {
+                                if no_client_dll_dir_name.is_dir() {
+                                    // copy the game dir
+                                    let copy_path =
+                                        no_client_dll_dir_name.join("Half-Life").join(game_name);
+
+                                    if !copy_path.is_dir() {
+                                        let copy_options = CopyOptions {
+                                            overwrite: true,
+                                            ..Default::default()
+                                        };
+                                        fs_extra::dir::copy(&game_dir, &copy_path, &copy_options)
+                                            .context(
+                                            "Failed to copy game dir to secondary game instance",
+                                        )?;
+                                    }
+                                } else {
+                                    bail!("Failed to find secondary game instance folder");
+                                }
+                            }
+                            None => {
+                                // generate toggle sim client batch files
+                                files::write_toggle_vanilla_game(&project_dir, &game_dir)?;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // create linker batch file
+            files::write_hltas_linker(&project_dir, &half_life_dir)?;
+
+            // TODO
+            // create run_game file
+
+            // copy cfg files on game if it doesn't exist and cfgs needs copying
+            let copy_cfg_files = {
+                if *copy_hltas_cfgs {
+                    true
+                } else if *no_copy_hltas_cfgs {
+                    false
+                } else {
+                    cfg.copy_cfgs_to_new_game
+                }
+            };
+
+            todo!()
+        }
         Commands::Init {
             folder_name,
             game_name,

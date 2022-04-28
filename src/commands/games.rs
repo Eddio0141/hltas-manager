27804221fc0d9d456cea480d818817627867d2bd
Eddio0141 <eddio0141@gmail.com>
2022-04-28 02:24:59 +0100

    games detection with more info

--- src/commands/games.rs
+++ src/commands/games.rs
@@ -7,33 +7,14 @@ pub fn games<P>(half_life_dir: P) -> Result<()>
 where
     P: AsRef<Path>,
 {
-    let games = games_in_dir(half_life_dir)?;
+    let games = game_dir_types(half_life_dir)?
+        .iter()
+        .map(|g| g.name)
+        .collect::<Vec<_>>();
 
     info!("Found {} games\n{}", games.len(), games.join("\n"));
 
     Ok(())
-}
-
-pub fn games_in_dir<P>(dir: P) -> Result<Vec<String>>
-where
-    P: AsRef<Path>,
-{
-    // TODO return references??
-    let dir = dir.as_ref();
-
-    let mut games = Vec::new();
-
-    for entry in fs::read_dir(dir).context("Failed to read directory")? {
-        let entry = entry.context("Failed to read directory")?;
-        let path = entry.path();
-
-        if path.is_dir() && is_dir_game(&path) {
-            let name = path.file_name().unwrap();
-            games.push(name.to_string_lossy().to_string());
-        }
-    }
-
-    Ok(games)
 }
 
 const HD_GAME: &str = "_hd";
@@ -41,24 +22,56 @@ const ADDON_GAME: &str = "_addon";
 const DLLS_DIR: &str = "dlls";
 const CL_DLLS_DIR: &str = "cl_dlls";
 
-pub fn is_dir_game<P>(path: P) -> bool
+pub struct DirType {
+    pub name: String,
+    pub has_hd: bool,
+    pub has_addon: bool,
+}
+
+pub fn game_dir_types<P>(dir: P) -> Result<Vec<DirType>>
 where
     P: AsRef<Path>,
 {
-    let path = path.as_ref();
+    let dir = dir.as_ref();
 
-    if path.is_file() {
-        return false;
-    }
+    let mut types = Vec::new();
 
-    match path.file_name() {
-        Some(file_name) => {
-            let file_name = file_name.to_string_lossy();
+    for entry in fs::read_dir(dir).context("Failed to read directory")? {
+        let entry = entry.context("Failed to read directory")?;
+        let path = entry.path();
 
-            !file_name.ends_with(HD_GAME)
-                && !file_name.ends_with(ADDON_GAME)
-                && (path.join(DLLS_DIR).is_dir() || path.join(CL_DLLS_DIR).is_dir())
+        if path.is_dir() {
+            let file_name = path
+                .file_name()
+                .context("Failed to get file name")?
+                .to_string_lossy();
+
+            let dir_type = types
+                .iter_mut()
+                .find(|t: &&mut DirType| t.name == file_name);
+            let dir_type = match dir_type {
+                Some(dir_type) => dir_type,
+                None => {
+                    if path.join(DLLS_DIR).is_dir() || path.join(CL_DLLS_DIR).is_dir() {
+                        types.push(DirType {
+                            name: file_name.to_string(),
+                            has_hd: false,
+                            has_addon: false,
+                        });
+                        types.last_mut().unwrap()
+                    } else {
+                        continue;
+                    }
+                }
+            };
+
+            if file_name.ends_with(HD_GAME) {
+                dir_type.has_hd = true;
+            } else if file_name.ends_with(ADDON_GAME) {
+                dir_type.has_addon = true;
+            }
         }
-        None => false,
     }
+
+    Ok(types)
 }

4d5d84c74482edac90f99cb27e65c65910d50887
Eddio0141 <eddio0141@gmail.com>
2022-05-06 18:55:08 +0100

    recommending use of ps1 version

--- src/files/files/bat/run_manager_base.bat
+++ src/files/files/bat/run_manager_base.bat
@@ -1,6 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo "I recommend using the powershell scripts instead by running 'init' here without '--bat'"
+echo "If you are using this then make sure you close TASView cuz I can't solve dumb thing with it"
+
 set args=
 
 for %%a in (%*) do (

2e41a3703f67bc1f3aa1561c100c8cfb318684c8
Eddio0141 <eddio0141@gmail.com>
2022-08-03 09:06:53 +0100

    install command work better

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -20,17 +20,27 @@ pub const STEAM_API_DLL_HASH: &[u8] = &[
     0x35, 0x7b, 0xe5, 0xc4, 0x95, 0xcc, 0xf6, 0xe0, 0x41, 0x58, 0x02, 0xb8, 0x6e, 0xae, 0x35, 0x34,
 ];
 
-pub fn install(
-    projects_dir: &Option<PathBuf>,
-    half_life_dir: &Option<PathBuf>,
-    minimum_cfgs: bool,
-) -> Result<()> {
-    info!("Loading config...");
+pub struct Override<'a> {
+    pub projects_dir: &'a Option<PathBuf>,
+    pub half_life_dir: &'a Option<PathBuf>,
+    pub minimum_cfgs: bool,
+    pub reset_cfgs: &'a Option<Vec<String>>,
+}
+
+pub fn install(override_: Override) -> Result<()> {
+    let Override {
+        projects_dir,
+        half_life_dir,
+        minimum_cfgs,
+        reset_cfgs,
+    } = override_;
+
     // config
+    info!("Loading manager config");
     let config_path = helper::cfg_dir()?;
     let cfg = cfg_file_set_up(
         config_path,
-        CfgOverrides {
+        ProjectCfgOverrides {
             projects_dir_name: projects_dir.as_deref(),
             half_life_dir: half_life_dir.as_deref(),
         },
@@ -40,21 +50,26 @@ pub fn install(
     let root_dir = exe_dir()?;
     let hl_dir = root_dir.join(&cfg.half_life_dir);
     let projects_dir = root_dir.join(&cfg.project_dir);
-    let sim_dll = "sim.dll";
-    let base_sim_client_dll_path = root_dir.join(sim_dll);
+    let sim_dll = "_sim.dll";
+    let reset_dll = "_reset.dll";
     let steam_api_dll = "steam_api.dll";
+    let base_sim_client_dll_path = root_dir.join(sim_dll);
     let steam_api_dll_path = hl_dir.join(steam_api_dll);
-    let reset_dll_path = hl_dir.join("reset.dll");
+    let reset_dll_path = hl_dir.join(reset_dll);
 
     // verifying if the half-life directory exists
+    info!("Verifying files");
     if !hl_dir.is_dir() {
-        bail!("Half-life directory does not exist, possible that you don't have the manager in a goldsrc package folder");
+        bail!("Half-life directory does not exist, possible that you don't have the manager in a GoldSrc Package folder");
     }
     // verifying that sim.dll exists in root dir
     if !base_sim_client_dll_path.is_file() {
         bail!("sim.dll does not exist in the root directory");
     }
-
+    // verifying if steam_api.dll exists
+    if !steam_api_dll_path.is_file() {
+        bail!("steam_api.dll does not exist in the Half-Life directory");
+    }
     // verify if steam_api.dll hash is matching
     let steam_api_dll_hash = helper::sha_256_file(&steam_api_dll_path)?;
 
@@ -68,20 +83,14 @@ pub fn install(
                 bail!("reset.dll hash is not matching default steam_api.dll hash");
             }
         } else {
-            warn!("steam_api.dll hash is not matching default steam_api.dll hash and reset.dll does not exist");
+            warn!("steam_api.dll hash is not matching default steam_api.dll hash and {reset_dll} does not exist, proceeding without simulator client dll");
         }
     }
 
     // create projects dir if it doesn't exist
     if !projects_dir.is_dir() {
-        if let Some(parent) = cfg.project_dir.parent() {
-            if parent != Path::new("") {
-                bail!("Projects directory needs to be inside the root dir without any directories in between");
-            }
-        }
-
         info!("Creating projects directory");
-        fs::create_dir(&projects_dir).context("Failed to create projects directory")?;
+        fs::create_dir_all(&projects_dir).context("Failed to create projects directory")?;
     }
 
     // copy half life directory if needs to be copied
@@ -114,9 +123,11 @@ pub fn install(
             }
         }
 
-        info!("Copying half-life directory to a second game folder");
+        info!("Partially copying half-life directory to a second game folder");
+        if !no_client_dll_dir.is_dir() {
+            fs::create_dir(&no_client_dll_dir).context("Failed to create second game folder")?;
+        }
 
-        fs::create_dir(&no_client_dll_dir).context("Failed to create second game folder")?;
         fs_extra::copy_items(
             copy_paths.as_slice(),
             &no_client_dll_dir,
@@ -142,38 +153,28 @@ pub fn install(
         .context("Failed to copy simulator dll to the second half-life directory")?;
     }
 
-    // hard link cfgs
-    cfgs_link(&root_dir, &cfg, minimum_cfgs)?;
+    // symbolic link cfgs
+    cfgs_link(&root_dir, &cfg, minimum_cfgs, reset_cfgs)?;
 
     // copy default steam_api.dll as reset.dll
-    // only on main half life directory
-    if steam_api_dll_path.is_file() {
-        if reset_dll_path.is_file() {
-            info!("reset.dll already exists, skipping");
-        } else {
-            info!("Copying default steam_api.dll to reset.dll");
-            fs::copy(&steam_api_dll_path, reset_dll_path)
-                .context("Failed to copy steam_api.dll to reset.dll")?;
-        }
+    // only do this on the main half life directory since the no client dll dir is used as the main client
+    if reset_dll_path.is_file() {
+        info!("reset.dll already exists, skipping");
     } else {
-        bail!("steam_api.dll not found in the Half-Life directory");
+        info!("Copying default steam_api.dll to reset.dll");
+        fs::copy(&steam_api_dll_path, reset_dll_path)
+            .context("Failed to copy steam_api.dll to reset.dll")?;
     }
 
     // copy the simulator client steam_api.dll (sim.dll)
-    // only do this on the main half life directory since the no client dll dir is used as the main client
-    if base_sim_client_dll_path.is_file() {
-        let sim_client_dll_path = hl_dir.join(sim_dll);
+    let sim_client_dll_path = hl_dir.join(sim_dll);
 
-        if sim_client_dll_path.exists() {
-            info!("sim.dll already exists in the Half-Life directory, proceeding copy anyway");
-        }
-
-        info!("Copying sim.dll to the game directory");
-        fs::copy(base_sim_client_dll_path, sim_client_dll_path)
-            .context("Failed to copy sim.dll")?;
-    } else {
-        bail!(format!("{sim_dll} not found in the root directory"));
+    if sim_client_dll_path.exists() {
+        info!("sim.dll already exists in the Half-Life directory, proceeding copy anyway");
     }
+
+    info!("Copying sim.dll to the game directory");
+    fs::copy(base_sim_client_dll_path, sim_client_dll_path).context("Failed to copy sim.dll")?;
 
     stop_tas_script(&root_dir, &cfg)?;
 
@@ -234,12 +235,12 @@ fn write_optim_rhai_script<P: AsRef<Path>>(root_dir: P, cfg: &Cfg) -> Result<()>
     Ok(())
 }
 
-struct CfgOverrides<'a> {
+struct ProjectCfgOverrides<'a> {
     projects_dir_name: Option<&'a Path>,
     half_life_dir: Option<&'a Path>,
 }
 
-fn cfg_file_set_up<P>(config_path: P, cfg_overrides: CfgOverrides) -> Result<Cfg>
+fn cfg_file_set_up<P>(config_path: P, cfg_overrides: ProjectCfgOverrides) -> Result<Cfg>
 where
     P: AsRef<Path>,
 {
@@ -247,7 +248,10 @@ where
 
     if !config_path.is_file() {
         // create the config file
-        Cfg::save_default_to_path(&config_path)?;
+        info!("Creating config file...");
+        Cfg::save_default_to_path(&config_path).with_context(|| {
+            format!("Failed to create config file at {}", config_path.display())
+        })?;
         info!("Created default config file");
     }
 
@@ -259,7 +263,7 @@ where
             Cfg::save_default_to_path(&config_path)?;
             let cfg = Cfg::load(&config_path)?;
 
-            info!("Couldn't load config file, saved default config file");
+            warn!("Couldn't load config file, saved default config file");
 
             cfg
         }
@@ -285,7 +289,12 @@ where
     Ok(cfg)
 }
 
-fn cfgs_link<P>(root_dir: P, cfg: &Cfg, minimum_cfgs: bool) -> Result<()>
+fn cfgs_link<P>(
+    root_dir: P,
+    cfg: &Cfg,
+    minimum_cfgs: bool,
+    reset_cfgs: &Option<Vec<String>>,
+) -> Result<()>
 where
     P: AsRef<Path>,
 {
@@ -297,13 +306,13 @@ where
         let cfgs_dir = root_dir.join(cfgs_dir);
 
         info!("Writing tas cfgs in root directory");
-        files::write_cfgs(&cfgs_dir, minimum_cfgs)?;
+        files::write_cfgs(&cfgs_dir, minimum_cfgs, reset_cfgs)?;
 
         // link to all half-life game directories
         for game_dir in game_dir_types(&half_life_dir)? {
             if !cfg.ignore_games.contains(&game_dir.name) {
                 info!(
-                    "Linking tas cfgs in first half-life dir game {}",
+                    "Linking tas cfgs in main Half-Life dir game {}",
                     game_dir.name
                 );
                 files::hard_link_cfgs(&cfgs_dir, half_life_dir.join(game_dir.name))?;
@@ -318,7 +327,7 @@ where
             for game_dir in game_dir_types(&no_client_dll_dir)? {
                 if !cfg.ignore_games.contains(&game_dir.name) {
                     info!(
-                        "Linking tas cfgs in second half-life dir game {}",
+                        "Linking tas cfgs in second Half-Life dir game {}",
                         game_dir.name
                     );
                     files::hard_link_cfgs(&cfgs_dir, no_client_dll_dir.join(game_dir.name))?;

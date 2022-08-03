1eecf991d183f17b9fc07a61400130e248326c21
Eddio0141 <eddio0141@gmail.com>
2022-08-03 09:20:14 +0100

    different file names

--- resource/bat/disable_sim_client.bat
+++ resource/bat/disable_sim_client.bat
@@ -5,11 +5,11 @@ cd ..\..\HALF_LIFE_DIR
 set half_life_dir=%CD%
 cd "%~dp0"
 
-set reset_steam_api_dll=_reset_steam_api.dll
+set reset_steam_api_dll=_reset.dll
 set steam_api_dll=steam_api.dll
 
 if exist "%half_life_dir%\%reset_steam_api_dll%" (
 	echo "using reset steam dll"
-	ren "%half_life_dir%\%steam_api_dll%" "_sim_steam_api.dll"
+	ren "%half_life_dir%\%steam_api_dll%" "_sim.dll"
 	ren "%half_life_dir%\%reset_steam_api_dll%" "%steam_api_dll%"
 )
--- resource/bat/enable_sim_client.bat
+++ resource/bat/enable_sim_client.bat
@@ -5,11 +5,11 @@ cd ..\..\HALF_LIFE_DIR
 set half_life_dir=%CD%
 cd "%~dp0"
 
-set sim_steam_api_dll=_sim_steam_api.dll
+set sim_steam_api_dll=_sim.dll
 set steam_api_dll=steam_api.dll
 
 if exist "%half_life_dir%\%sim_steam_api_dll%" (
 	echo "using simulator client steam dll"
-	ren "%half_life_dir%\%steam_api_dll%" "_reset_steam_api.dll"
+	ren "%half_life_dir%\%steam_api_dll%" "_reset.dll"
 	ren "%half_life_dir%\%sim_steam_api_dll%" "%steam_api_dll%"
 )

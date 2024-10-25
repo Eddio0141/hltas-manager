4d14ad88e3873fe5588fb4756d45e600f0a7677a
Eddio0141 <eddio0141@gmail.com>
2024-10-25 17:08:25 +0100

    removed unused batch files

--- resource/bat/disable_vanilla_game.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "game_dir=GAME_DIR"
-set "client_dll_full=%game_dir%\vanilla_client.dll"
-
-if exist %client_dll_full% (
-	ren "%client_dll_full%" client.dll
-)
--- resource/bat/enable_vanilla_game.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "game_dir=GAME_DIR"
-set "client_dll_full=%game_dir%\client.dll"
-
-if exist %client_dll_full% (
-	ren "%client_dll_full%" vanilla_client.dll
-)

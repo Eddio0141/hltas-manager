661a6e51af5c8d6c504defefcd08d0007761131c
Eddio0141 <eddio0141@gmail.com>
2025-03-25 21:42:20 +0000

    use RUST_LOG, debug print cli args

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
 // use ansi_term::Colour::Red;
 use hltas_manager::{cli::Cli, commands::run};
-use log::{error, LevelFilter};
+use log::{debug, error, LevelFilter};
 use std::io::Write;
 
 fn main() {
     let cli = Cli::parse();
 
     init_logger(&cli);
+
+    debug!("cli: {cli:#?}");
 
     if let Err(err) = run(cli) {
         error!("{:?}", err);
@@ -28,11 +30,13 @@ fn init_logger(cli: &Cli) {
     #[cfg(debug_assertions)]
     builder
         .format_timestamp(Some(env_logger::TimestampPrecision::Millis))
-        .filter_level(LevelFilter::Debug);
+        .filter_level(LevelFilter::Debug)
+        .parse_default_env();
     #[cfg(not(debug_assertions))]
     builder
         .format_timestamp(None)
-        .filter_level(LevelFilter::Info);
+        .filter_level(LevelFilter::Info)
+        .parse_default_env();
 
     builder.format(move |buf, record| writeln!(buf, "{}: {}", record.level(), record.args()));
 
